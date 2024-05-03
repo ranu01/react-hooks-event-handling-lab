bd7f0f24508ae46ccbc8f5be5a4d2bc0f0292863
lewis karanu <lewiskaranu0@gmail.com>
2024-05-04 01:35:30 +0300

    add

---
+++ src/components/EyeOnMe.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const handleOnFocus = () => {
+  console.log("Good!");
+};
+const handleOnBlur = () => {
+  console.log("Hey! Eyes on me!");
+};
+function EyesOnMe() {
+  return (
+    <button onFocus={handleOnFocus} onBlur={handleOnBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
---
+++ src/components/Keypad.jsx
@@ -0,0 +1,12 @@
+function Keypad() {
+  const handleChange = () => {
+    console.log("Entering password...");
+  };
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
+}
+
+export default Keypad;
